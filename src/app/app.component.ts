5fd77e88091a2a17046bed43fa76edd7192b7033
sonatedev@outlook.com <sonatedev@outlook.com>
2022-03-04 09:09:54 -0500

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as signalR from "@microsoft/signalr";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,73 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'SignalR';
+  data:any;
+  lista:Array<any>;
+  token: string;
+  latitude: string;
+  longitude: string;
+
+  private hubConnection: signalR.HubConnection;
+  constructor(){
+    this.hubConnection = new signalR.HubConnectionBuilder()
+    .withUrl('https://bomberosnetcol.azurewebsites.net/notifications/localization',{
+      accessTokenFactory: () => this.getAccessToken()
+    })
+    .build();
+    this.lista = [];
+    this.token = "";
+    this.latitude = "22222";
+    this.longitude = "222";
+  }
+
+  getAccessToken(): string {
+    return this.token;   }
+  public startConnection = () => {
+    this.hubConnection
+      .start()
+      .then(() => console.log('Connection started'))
+      .catch(err => console.log('Error while starting connection: ' + err))
+  }
+
+
+  public addTransferChartDataListener = () => {
+    console.log("getting");
+    this.hubConnection.on('NotificationFromServer', (data,fulldata) => {
+      console.log(data);
+      console.log(fulldata);
+      this.lista.push(fulldata);
+    });
+
+
+  }
+
+  senData(){
+    var coordinat = new GeographicalCoordinates("ssss","ssss");
+    var local = new UserLocalization("sscxxx", coordinat);
+    console.log("sending");
+    this.hubConnection.invoke("SendLocationDataToAllAsync", this.latitude, this.longitude).catch(err=> console.log(err))
+  }
+
 }
+
+export class UserLocalization{
+
+  constructor(userId: string, geographicalCoordinates: GeographicalCoordinates){
+    this.userId = userId;
+    this.geographicalCoordinates = geographicalCoordinates;
+  }
+  userId: string;
+  geographicalCoordinates: GeographicalCoordinates;
+
+}
+export class GeographicalCoordinates{
+  constructor(latitude:string, longitude: string) {
+    this.latitude = latitude,
+    this.longitude = longitude
+  }
+  latitude: string;
+  longitude: string;
+}
+
